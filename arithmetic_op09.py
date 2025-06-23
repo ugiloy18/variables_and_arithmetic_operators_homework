2b31df4b07e3516f5eebc975db5a88c799dfbee0
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-23 10:47:13 +0500

    saqlandi

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -1,9 +1,10 @@
 # Create a variable "a"  and assign it the value of the int 8.
-
+a=8
 # Create a variable "b"  and assign it the value of the int 3.
-
+b=3
 # Create a variable "c"  and assign it the value of the int 2.
-
+c=2
 # Create a variable "d"  and assign it the value of the int 4.
-
+d=4
+answer=a+b-c*d
 # Create a variable called 'answer' and assign it the expression value: a+b - cd
