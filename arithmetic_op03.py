1b89401406e9e5cf19ab346c5c726b97728780eb
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-23 10:42:09 +0500

    saqlandi

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,7 @@
 # Create a variable "a"  and assign it the value of the int 6.
-
+a=6
 # Create a variable "b"  and assign it the value of the int 2.
-
+b=2
+answer=a-b
+print(answer)
 # Create a variable called 'answer' and assign it the difference of a and b.
